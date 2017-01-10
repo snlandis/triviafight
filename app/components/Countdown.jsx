3080061d7f18aea224eb0ab96fecfb1d29b49c51
luke popwell <lukepopwell@mac.com>
2017-01-10 09:36:51 -0600

    changed clock css and removed h1 from clock component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -64,7 +64,6 @@ var Countdown = React.createClass({
 
 		return (
 			<div>
-				<h1 className="page-title">Countdown App</h1>
 				<Clock totalSeconds={count}/>
 				{renderControlArea()}
 			</div>

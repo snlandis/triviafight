02cfb1ada3561837afcce9af9573894dd98f0b12
luke popwell <lukepopwell@mac.com>
2017-01-11 16:41:55 -0600

    added Gameroom.jsx

---
+++ app/components/Gameroom.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var {Link} = require('react-router');
+
+
+var Gameroom = React.createClass({
+  render: function() {
+    return (
+      <div className="gameroom">
+          <p>lots of words and shit goes here.....</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Gameroom;

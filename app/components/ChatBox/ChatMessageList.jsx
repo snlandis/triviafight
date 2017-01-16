bdbe6aef04448aa7351a57a92c5f27fe6e1a8388
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-01-16 12:14:45 -0600

    removed all info about the bot

--- app/components/ChatBox/ChatMessageList.jsx
+++ app/components/ChatBox/ChatMessageList.jsx
@@ -3,9 +3,6 @@ import firebase from 'firebase'
 import ChatMessage from './ChatMessage'
 import ChatInput from './ChatInput'
 
-const BOT_AVATAR = 'https://firebasestorage.googleapis.com/v0/b/react-firebase-chat-74da6.appspot.com/o/img%2Fpaje_real.png?alt=media&token=7c2a5fd4-09ac-4997-8c50-ae9e1de8fca7'
-const BOT_NAME = 'Big Dave'
-
 class ChatMessageList extends Component {
   static propTypes = {
     user: PropTypes.object.isRequired
@@ -14,9 +11,7 @@ class ChatMessageList extends Component {
   constructor (props) {
     super(props)
     this.messagesDB = firebase.database().ref(`messages/`)
-
   /*  this is the original string this.messagesDB = firebase.database().ref(`messages/${this.props.user.uid}`) */
-
     this.handleSendMessage = this.handleSendMessage.bind(this)
   }
 

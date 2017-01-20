ee71bc1698443db6d52c1e8fc1d4635a41c902b8
luke popwell <lukepopwell@mac.com>
2017-01-20 13:43:11 -0600

    fixed conflicts

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,21 +3,13 @@ var React = require('react');
 var Footer = React.createClass({
   render : function(){
     return (
-<<<<<<< HEAD
       <footer className="challenge-footer">
         <h6 className="foot createdby">
           <a href="https://github.com/kcricht06"><img border="0" alt="W3Schools" src="app/images/evanderholyfield.png" className="img-circle" width="100" height="100" /></a>
           <a href="https://github.com/ZHamburglar"><img border="0" alt="W3Schools" src="app/images/miketyson.png" className="img-circle" width="100" height="100" /></a>
           <a href="https://github.com/Luke-Popwell"><img border="0" alt="W3Schools" src="app/images/oscardelahoya.png" className="img-circle" width="100" height="100" /></a>
           <a href="https://github.com/snlandis"><img border="0" alt="W3Schools" src="app/images/royjonesjr.png" className="img-circle" width="100" height="100" /></a>
-=======
-      <div className="font main">
-        <h6 className="foot createdby">
-          <a href="https://github.com/kcricht06"><img border="0" alt="Kyle Crichton" src="app/images/evanderholyfield.png" className="image img-circle" width="100" height="100"/></a>
-          <a href="https://github.com/ZHamburglar"><img border="0" alt="Kollin Brandenburg" src="app/images/miketyson.png" className="image img-circle" width="100" height="100" /></a>
-          <a href="https://github.com/Luke-Popwell"><img border="0" alt="Luke Popwell" src="app/images/oscardelahoya.png" className="image img-circle" width="100" height="100" /></a>
-          <a href="https://github.com/snlandis"><img border="0" alt="Stephen Landis" src="app/images/royjonesjr.png" className="image img-circle" width="100" height="100" /></a>
->>>>>>> 9b466982cb83c5c996f4eba0541889366d320b0e
+
         </h6>
       </footer>
     );
